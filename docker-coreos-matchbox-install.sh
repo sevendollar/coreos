d5deab14fa85d9f34e0baa8b4bf29b98f580122b
sevendollar <sevendollar@gmail>
2017-09-01 16:44:32 +0800

    tunning the script

--- docker-coreos-matchbox-install.sh
+++ docker-coreos-matchbox-install.sh
@@ -8,4 +8,5 @@ export SAN=DNS.1:matchbox.cvl.com.tw,IP.1:10.5.1.5 && \
 ./cert-gen && \
 sudo mkdir -p /etc/matchbox && \
 sudo cp ca.crt server.crt server.key client.crt client.key /etc/matchbox && \
+cd ../../.. && rm -rf matchbox-v0.6.1-linux-amd64 && \
 sudo mkdir -p /var/lib/matchbox/assets
