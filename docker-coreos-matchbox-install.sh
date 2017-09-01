8a1b282c37070990f07eaca8d74451c23861d860
sevendollar <sevendollar@gmail>
2017-09-01 14:21:47 +0800

    split docker-coreos-matchbox.sh in to docker-coreos-matchbox.sh & docker-coreos-matchbox-install.sh

---
+++ docker-coreos-matchbox-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# matchbox v0.6.1
+rm -rf {matchbox-v0.6.1-linux-amd64,matchbox-v0.6.1-linux-amd64.tar.gz} && \
+curl -LO https://github.com/coreos/matchbox/releases/download/v0.6.1/matchbox-v0.6.1-linux-amd64.tar.gz && \
+tar zxvf matchbox-v0.6.1-linux-amd64.tar.gz && rm -rf matchbox-v0.6.1-linux-amd64.tar.gz && \
+cd matchbox-v0.6.1-linux-amd64/scripts/tls/ && \
+export SAN=DNS.1:matchbox.cvl.com.tw,IP.1:10.5.1.5 && \
+./cert-gen && \
+sudo mkdir -p /etc/matchbox && \
+sudo cp ca.crt server.crt server.key client.crt client.key /etc/matchbox && \
+sudo mkdir -p /var/lib/matchbox/assets
