ac1a1e6b5e334bcd2768c7cd319ea0a880f685af
sevendollar <sevendollar@gmail>
2017-09-12 10:42:11 +0800

    put togather etc3-examples to folder

--- run-etcd-1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -LO http://10.5.15.66:32772/etcd-1.json
-sudo coreos-install -d /dev/sda -C stable -i etcd-1.json -o vmware_raw
-sudo systemctl reboot
--- run-matchbox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./docker-coreos-matchbox-install.sh && \
-./docker-coreos-matchbox.sh && \
-./docker-coreos-dnsmasq.sh
