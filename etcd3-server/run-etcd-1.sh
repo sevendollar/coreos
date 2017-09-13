5392a7d8ffbff105c92ecb1193e0cb3cd4c216e6
sevendollar <sevendollar@gmail>
2017-09-13 17:23:35 +0800

    etcd tls first try

--- etcd3-server/run-etcd-1.sh
+++ etcd3-server/run-etcd-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -LO http://10.5.15.66:32772/etcd-1.json
+curl -LO http://10.5.15.66:32772/etcd3-server/etcd-1.json
 sudo coreos-install -d /dev/sda -C stable -i etcd-1.json -o vmware_raw
 sudo systemctl reboot
--- etcd3-server/run-etcd-2.sh
+++ etcd3-server/run-etcd-2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -LO http://10.5.15.66:32772/etcd-2.json
+curl -LO http://10.5.15.66:32772/etcd3-server/etcd-2.json
 sudo coreos-install -d /dev/sda -C stable -i etcd-2.json -o vmware_raw
 sudo systemctl reboot
